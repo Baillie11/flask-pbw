06825f8e290af9fc11201752b574018cff0cc39f
Andrew Baillie <andrewgbaillie@hotmail.com>
2022-02-01 21:21:54 +1100

    First Commit

---
+++ app.py
@@ -0,0 +1,68 @@
+from flask import Flask, render_template, url_for, request
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecretkey'
+
+class pbwForm(FlaskForm):
+    
+    clientID = StringField('Client ID:',validators=[DataRequired()])
+    retrieve = SubmitField('Retrieve from NAB Transact')
+    businessName = StringField('Trading Name:')
+    abn = StringField('ABN:')
+    billerID = StringField('Biller ID:')
+    note= StringField('Note:')
+    type= SelectField('Type:', choices=['Council', 'School', 'Utility', 'ISP', 'Other'])
+    state= SelectField('State:', choices=['ACT', 'NSW', 'NT', 'QLD', 'SA', 'TAS', 'VIC', 'WA'])
+    submit = SubmitField('Create Biller')
+    
+
+    
+    
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+@app.route('/upload',methods=['POST', 'GET'])
+def upload():
+    return render_template("upload.html")
+
+@app.route('/pbwform', methods=['POST', 'GET'])
+def pbwform():
+    businessName=None
+    clientID=None
+    form = pbwForm()
+    return render_template("pbwform.html",form=form)
+        
+
+
+@app.route('/result',methods=['POST', 'GET'])
+def result():
+    output = request.form.to_dict()
+    name = output["name"]
+    invoice = output['invoice']
+    linvoice = invoice.lower() #convert to lowercase
+    npinvoice = linvoice.replace(" ","") #remove spaces
+    Linvoice = npinvoice[:10] #get first 10 characters
+    return render_template('index.html', name = name, invoice = Linvoice)
+    PATH = "C:\Program Files (x86)\chromedriver.exe"
+    driver=webdriver.Chrome(PATH)
+    driver.get("https://www.google.com/")
+
+
+
+
+
+
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/main.js
@@ -0,0 +1,87 @@
+const canvas = document.getElementById("result");
+const ctx = canvas.getContext("2d");
+//const originalName=""
+
+
+function loadImage(event) {
+  originalName=file.files[0].name;
+  const image = document.getElementById("imgDisplayed");
+  image.src = URL.createObjectURL(event.target.files[0]);
+  image.onload = function() {
+  originalWidth=this.width;   
+  originalHeight=this.height;
+  console.log("Orginal Width=" + originalWidth);
+  console.log("Orginal Height=" + originalHeight);
+            
+  document.getElementById("imWidth").value=this.width;
+  document.getElementById("imHeight").value=originalHeight;
+
+  prev();
+    }
+
+
+//const preview = document.getElementById("preview");
+//preview.addEventListener("click", prev);
+
+
+function prev() {
+    const image = document.getElementById("imgDisplayed");
+    const imWidth = document.getElementById("imWidth").value;
+    const imHeight = document.getElementById("imHeight").value;
+    console.log("prev:imWidth=" + imWidth);
+    console.log("prev:imHeight=" + imHeight);
+
+    canvas.width = imWidth;
+    canvas.height = imHeight;
+
+    ctx.drawImage(image, 0, 0, imWidth, imHeight);
+
+   // form = document.querySelector("form");
+   // form.reset();
+}
+
+
+const down = document.querySelector("#down");
+down.addEventListener("click", downloadImage);
+
+function downloadImage(){
+    console.log(originalName);
+  if(window.navigator.msSaveBlob) {
+    window.navigator.msSaveBlob(canvas.msToBlob(), originalName);
+    
+
+  } else {
+    
+    const a = document.createElement("a");
+    document.body.appendChild(a);
+    a.href = canvas.toDataURL();
+
+    a.download = originalName;
+    a.click();
+
+    document.body.removeChild(a);
+  }
+  //This line clears the form and resets the canvas for the next image
+  //window.location.reload();
+}
+
+//Read slider values
+const slider = document.getElementById("myRange");
+const output = document.getElementById("demo");
+slider.oninput = function() {
+output.innerHTML = slider.value;
+console.log(slider.value);
+//originalWidth=document.getElementById("imWidth").value;
+//originalHeight=document.getElementById("imHeight").value;
+
+iwidth = originalWidth/100*slider.value;
+iheight = originalHeight/100*slider.value;
+
+//console.log('Width: '+parseInt(iwidth));
+//console.log('Height: '+parseInt(iheight));
+document.getElementById("imWidth").value=parseInt(iwidth);
+document.getElementById("imHeight").value=parseInt(iheight);
+
+prev();
+};
+};
